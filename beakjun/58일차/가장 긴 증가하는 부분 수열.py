6ddfa77afeabaadb5bfab07ee14d94891674e6e8
HYUNMIN YOO <2049905@naver.com>
2022-04-14 13:19:36 +0900

    58일차

---
+++ beakjun/58일차/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,19 @@
+N = int(input())
+a = list(map(int, input().split()))
+dp = [1] * (N+1)
+for i in range(N):
+    for j in range(i):
+        if a[i] > a[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
+
+max_dp = max(dp)
+max_idx = dp.index(max_dp)
+l = list()
+
+while max_idx >= 0:
+    if dp[max_idx] == max_dp:
+        l.append(a[max_idx])
+        max_dp -= 1
+    max_idx -= 1
+print(*reversed(l))
