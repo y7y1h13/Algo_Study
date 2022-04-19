afd8cdba2ade3524fb06497a5557e853e4a08858
HYUNMIN YOO <2049905@naver.com>
2022-04-19 11:40:32 +0900

    61일차

---
+++ beakjun/61일차/일곱 난쟁이.py
@@ -0,0 +1,7 @@
+from itertools import combinations
+
+a = list(int(input()) for _ in range(9))
+for i in combinations(a, 7):
+    if sum(i) == 100:
+        print(*sorted(i), sep='\n')
+        break
