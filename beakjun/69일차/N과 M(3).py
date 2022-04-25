7597842988b671037706b309b9d76f28046645e3
HYUNMIN YOO <2049905@naver.com>
2022-04-25 15:00:02 +0900

    69일차

---
+++ beakjun/69일차/N과 M(3).py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+a = []
+
+
+def dfs():
+    if len(a) == M:
+        print(' '.join(map(str, a)))
+        return
+    for i in range(1, N + 1):
+        a.append(i)
+        dfs()
+        a.pop()
+
+
+dfs()
---
+++ beakjun/69일차/N과 M(4).py
@@ -0,0 +1,15 @@
+N, M = map(int, input().split())
+a = []
+
+
+def dfs(n):
+    if len(a) == M:
+        print(' '.join(map(str, a)))
+        return
+    for i in range(n, N + 1):
+        a.append(i)
+        dfs(i)
+        a.pop()
+
+
+dfs(1)
