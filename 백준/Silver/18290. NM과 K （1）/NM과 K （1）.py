78086ae7204181222bb5befa66ce6f7839c5241f
HYUNMIN YOO <2049905@naver.com>
2022-04-27 11:27:07 +0900

    [Silver I] Title: NM과 K (1), Time: 4632 ms, Memory: 118024 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
@@ -1,6 +1,3 @@
-import math
-
-
 def dfs(x, y):
     global ans
     if len(q) == K:
@@ -9,18 +6,19 @@ def dfs(x, y):
             tmp += a[i[0]][i[1]]
         ans = max(ans, tmp)
         return
-    for i in range(x, N):
-        for j in range(y if i == x else 0, M):
-            if [i, j] not in q:
-                if ([i+1, j] not in q) and ([i-1, j] not in q) and ([i, j+1] not in q) and ([i, j-1] not in q):
-                    q.append([i, j])
-                    dfs(i, j)
-                    q.pop()
+    else:
+        for i in range(x, N):
+            for j in range(y if i == x else 0, M):
+                if [i, j] not in q:
+                    if ([i + 1, j] not in q) and ([i - 1, j] not in q) and ([i, j + 1] not in q) and ([i, j - 1] not in q):
+                        q.append([i, j])
+                        dfs(i, j)
+                        q.pop()
 
 
 N, M, K = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(N)]
 q = []
-ans = -math.inf
+ans = -1e10
 dfs(0, 0)
 print(ans)
