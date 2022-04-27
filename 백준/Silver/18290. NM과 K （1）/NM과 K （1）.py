7a34fcf362be204ee4c81007fca2ceba142ca66c
HYUNMIN YOO <2049905@naver.com>
2022-04-27 11:13:43 +0900

    [Silver I] Title: NM과 K (1), Time: 7396 ms, Memory: 120168 KB -BaekjoonHub

---
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.py
@@ -0,0 +1,26 @@
+import math
+
+
+def dfs(x, y):
+    global ans
+    if len(q) == K:
+        tmp = 0
+        for i in q:
+            tmp += a[i[0]][i[1]]
+        ans = max(ans, tmp)
+        return
+    for i in range(x, N):
+        for j in range(M):
+            if [i, j] not in q:
+                if ([i+1, j] not in q) and ([i-1, j] not in q) and ([i, j+1] not in q) and ([i, j-1] not in q):
+                    q.append([i, j])
+                    dfs(i, j)
+                    q.pop()
+
+
+N, M, K = map(int, input().split())
+a = [list(map(int, input().split())) for _ in range(N)]
+q = []
+ans = -math.inf
+dfs(0, 0)
+print(ans)
