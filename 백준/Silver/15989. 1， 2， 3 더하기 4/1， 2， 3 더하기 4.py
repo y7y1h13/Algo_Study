d7b946d0f352118f5ace279541f1c56d013497c4
HYUNMIN YOO <2049905@naver.com>
2022-04-29 14:29:43 +0900

    [Silver I] Title: 1, 2, 3 더하기 4, Time: 92 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.py
@@ -0,0 +1,10 @@
+T = int(input())
+a = list(int(input()) for _ in range(T))
+dp = [1] * (max(a) + 1)
+for i in range(2, len(dp)):
+    dp[i] += dp[i - 2]
+for i in range(3, len(dp)):
+    dp[i] += dp[i - 3]
+
+for i in a:
+    print(dp[i])
