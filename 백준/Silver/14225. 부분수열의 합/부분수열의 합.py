a4a1d5a687ef020fd3b968ef95cfdafdae35c30f
HYUNMIN YOO <2049905@naver.com>
2022-05-03 11:35:37 +0900

    [Silver I] Title: 부분수열의 합, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.py
@@ -1,5 +1,4 @@
 import sys
-from itertools import combinations
 
 
 input = sys.stdin.readline
@@ -7,9 +6,15 @@ input = sys.stdin.readline
 N = int(input())
 a = list(map(int, input().split()))
 a.sort()
-ans = set([i for i in range(1, N*max(a) + 2)])
-for i in range(1, N+1):
-    for j in combinations(a, i):
-        if sum(j) in ans:
-            ans.discard(sum(j))
-print(min(ans))
+for i in range(N):
+    if i == 0:
+        if a[i] == 1:
+            continue
+        else:
+            print(1)
+            break
+    if sum(a[:i]) + 1 < a[i]:
+        print(sum(a[:i]) + 1)
+        break
+else:
+    print(sum(a, 1))
