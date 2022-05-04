f675f977bb08b938f64563616299af9332474dd0
HYUNMIN YOO <2049905@naver.com>
2022-05-04 09:22:36 +0900

    [Silver II] Title: 연산자 끼워넣기 (2), Time: 248 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15658. 연산자 끼워넣기 （2）/연산자 끼워넣기 （2）.py
@@ -0,0 +1,41 @@
+def dfs(depth, total):
+    global Max
+    global Min
+    if depth == (N-1):
+        if Max < total:
+            Max = total
+
+        if Min > total:
+            Min = total
+        return
+
+    for i in range(4):
+        if i == 0 and op[i]:
+            op[i] -= 1
+            dfs(depth + 1, total + a[depth + 1])
+            op[i] += 1
+        elif i == 1 and op[i]:
+            op[i] -= 1
+            dfs(depth + 1, total - a[depth + 1])
+            op[i] += 1
+        elif i == 2 and op[i]:
+            op[i] -= 1
+            dfs(depth + 1, total * a[depth + 1])
+            op[i] += 1
+        elif i == 3 and op[i]:
+            op[i] -= 1
+            if total >= 0:
+                dfs(depth + 1, total // a[depth + 1])
+            else:
+                dfs(depth + 1, -(-total // a[depth + 1]))
+            op[i] += 1
+
+
+N = int(input())
+a = list(map(int, input().split()))
+op = list(map(int, input().split()))
+Max = -1e10
+Min = 1e10
+dfs(0, a[0])
+print(Max)
+print(Min)
