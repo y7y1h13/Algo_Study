4c83881b1b8d7aa1c0b02a04b94e446b6cbb05d7
HYUNMIN YOO <2049905@naver.com>
2022-05-04 12:14:34 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 4116 ms, Memory: 34592 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.py
@@ -4,48 +4,52 @@ from collections import deque
 sys.setrecursionlimit(10 ** 9)
 
 
+def make_graph(N):
+    a = {}
+    for i in range(N):
+        a[i] = list()
+
+    for _ in range(N):
+        i, j = map(int, input().split())
+        a[i - 1].append(j - 1)
+        a[j - 1].append(i - 1)
+    return a
+
+
 def dfs(start, cnt, now):
     visited[now] = 0
     if start == now and cnt >= 2:
-        check[start] = True
+        ans[start] = 0
         return
-    for i in a[now]:
+    for i in graph[now]:
         if visited[i]:
-
             dfs(start, cnt + 1, i)
         elif i == start and cnt >= 2:
-            dfs(start, cnt, i)
+            ans[start] = 0
+            return
 
 
 def bfs():
     q = deque()
     for i in range(N):
-        if check[i]:
+        if not ans[i]:
             q.append(i)
-            ans[i] = 0
 
     while q:
         now = q.popleft()
-        for i in a[now]:
+        for i in graph[now]:
             if ans[i] == -1:
                 q.append(i)
                 ans[i] = ans[now] + 1
 
 
 N = int(input())
-a = {}
+graph = make_graph(N)
+ans = [-1] * N
 
-for i in range(N):
-    a[i] = list()
 
-for _ in range(N):
-    i, j = map(int, input().split())
-    a[i - 1].append(j - 1)
-    a[j - 1].append(i - 1)
-check = [False] * N
 for start in range(N):
     visited = [1] * N
     dfs(start, 0, start)
-ans = [-1] * N
 bfs()
 print(*ans)
