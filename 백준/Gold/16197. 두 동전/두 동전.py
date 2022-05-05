2360b89f7fa726f564bbbdedf13f82b464bb89d7
HYUNMIN YOO <2049905@naver.com>
2022-05-05 10:56:51 +0900

    [Gold IV] Title: 두 동전, Time: 1232 ms, Memory: 121572 KB -BaekjoonHub

--- 백준/Gold/16197. 두 동전/두 동전.py
+++ 백준/Gold/16197. 두 동전/두 동전.py
@@ -7,16 +7,14 @@ def bfs(x, y, w, z, cnt):
 
     while q:
         x1, y1, x2, y2, cnt = q.popleft()
-        if cnt + 1 > 10:
+        if cnt >= 10:
             return -1
         for i in range(4):
             nx1 = x1 + dx[i]
             ny1 = y1 + dy[i]
             nx2 = x2 + dx[i]
             ny2 = y2 + dy[i]
-            if nx1 == nx2 and ny1 == ny2:
-                continue
-            elif 0 <= nx1 < N and 0 <= ny1 < M and 0 <= nx2 < N and 0 <= ny2 < M:
+            if 0 <= nx1 < N and 0 <= ny1 < M and 0 <= nx2 < N and 0 <= ny2 < M:
                 if a[nx1][ny1] == '#':
                     nx1, ny1 = x1, y1
                 if a[nx2][ny2] == '#':
@@ -28,7 +26,6 @@ def bfs(x, y, w, z, cnt):
                 return cnt + 1
             else:
                 continue
-    return -1
 
 
 N, M = map(int, input().split())
