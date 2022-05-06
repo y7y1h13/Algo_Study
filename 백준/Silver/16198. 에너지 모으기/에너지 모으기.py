4f477fd49d46170607a37843316f9c1d7b2301fb
HYUNMIN YOO <2049905@naver.com>
2022-05-06 09:33:24 +0900

    [Silver I] Title: 에너지 모으기, Time: 132 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
+++ 백준/Silver/16198. 에너지 모으기/에너지 모으기.py
@@ -1,18 +1,14 @@
 def dfs(tmp):
     global ans
-    if len(a) == 2:
-        if ans < tmp:
-            ans = tmp
+    if len(a)==2:
+        ans=max(ans,tmp)
         return
-    for i in range(1, len(a) - 1):
-        k = a[i]
+    for i in range(1,len(a)-1):
+        k=a[i]
         del a[i]
-        dfs(tmp + a[i - 1] * a[i])
-        a.insert(i, k)
-
-
-N = int(input())
-a = list(map(int, input().split()))
-ans = 0
+        dfs(tmp+a[i-1]*a[i])
+        a.insert(i,k)
+N,a=int(input()),list(map(int,input().split()))
+ans=0
 dfs(0)
 print(ans)
