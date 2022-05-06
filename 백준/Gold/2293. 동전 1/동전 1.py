a7f4b7df51a5aeca78e855d31fbfd0b68562263f
HYUNMIN YOO <2049905@naver.com>
2022-05-06 14:35:29 +0900

    [Gold V] Title: 동전 1, Time: 180 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,12 +1,15 @@
 import sys
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
-dp = [1] + [0] * k
-for _ in range(n):
-    c = int(input())
-    for j in range(c, k + 1):
-        if dp[j - c] == 0:
-            continue
-        dp[j] += dp[j - c]
-print(dp[-1])
+
+def sol():
+    n, k = map(int, input().split())
+    dp = [1] + [0] * k
+    for _ in range(n):
+        c = int(input())
+        for j in range(c, k + 1):
+            if dp[j - c] == 0:
+                continue
+            dp[j] += dp[j - c]
+    print(dp[-1])
+sol()
