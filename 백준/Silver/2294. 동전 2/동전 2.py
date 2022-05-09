7867d263d60479973b6efabc2f386dbc2acf049c
HYUNMIN YOO <2049905@naver.com>
2022-05-09 12:48:59 +0900

    [Silver I] Title: 동전 2, Time: 408 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2294. 동전 2/동전 2.py
+++ 백준/Silver/2294. 동전 2/동전 2.py
@@ -4,7 +4,7 @@ dp = [10001] * (k + 1)
 dp[0] = 0
 for i in a:
     for j in range(i, k + 1):
-        dp[j] = min(dp[j], dp[j - i] + 1)
+        dp[j] = dp[j] if dp[j] < dp[j - i] + 1 else dp[j - i] + 1
 if dp[j] > 10000:
     print(-1)
     exit()
