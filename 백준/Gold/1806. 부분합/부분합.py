25e3eacb1954006cb6747155a66bf2e80f0534ec
HYUNMIN YOO <2049905@naver.com>
2022-05-12 09:40:09 +0900

    [Gold IV] Title: 부분합, Time: 120 ms, Memory: 41620 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -3,23 +3,27 @@ import sys
 input = sys.stdin.readline
 
 
+def solution():
+    s, e = 0, 0
+    ans = 1e5
+    n_s = 0
+    while s < N:
+        if n_s >= S:
+            if ans > e - s:
+                ans = e - s
+            n_s -= a[s]
+            s += 1
+        elif e == N:
+            break
+        else:
+            n_s += a[e]
+            e += 1
+    if ans == 1e5:
+        print(0)
+        exit()
+    print(ans)
+
+
 N, S = map(int, input().split())
 a = list(map(int, input().split()))
-s, e = 0, 0
-ans = 1e5
-n_s = 0
-while s < N:
-    if n_s >= S:
-        if ans > e - s:
-            ans = e - s
-        n_s -= a[s]
-        s += 1
-    elif e == N:
-        break
-    else:
-        n_s += a[e]
-        e += 1
-if ans == 1e5:
-    print(0)
-else:
-    print(ans)
+solution()
