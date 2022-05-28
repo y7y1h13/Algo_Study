7d65e8ea9a18acc00621a255074fe48d76b4cf71
HYUNMIN YOO <2049905@naver.com>
2022-05-28 11:07:51 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 2696 ms, Memory: 32440 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.py
@@ -1,34 +1,38 @@
 from collections import deque
 
-N, K = map(int, input().split())
-a = list(map(int, input().split()))
 
-robot = deque()
-robot.extend([0] * N)
+def solution():
+    N, K = map(int, input().split())
+    a = list(map(int, input().split()))
 
-belt = deque()
-belt.extend(a)
+    robot = deque([0] * N)
+    belt = deque(a)
 
-ans = 0
-while belt.count(0) < K:
-    # 한 칸 회전
-    belt.appendleft(belt.pop())
-    robot.appendleft(robot.pop())
+    ans = 0
+    while belt.count(0) < K:
+        # 한 칸 회전
+        belt.rotate(1)
+        robot.rotate(1)
 
-    # 로봇 내리기
-    robot[N - 1] = 0
+        # 로봇 내리기
+        robot[N - 1] = 0
 
-    # 로봇 이동
-    for i in range(N - 2, 0, -1):
-        if robot[i] and not robot[i + 1] and belt[i + 1]:
-            robot[i + 1] = 1
-            robot[i] = 0
-            belt[i + 1] -= 1
-        robot[-1] = 0
+        # 로봇 이동
+        for i in range(N - 2, 0, -1):
+            if robot[i] and not robot[i + 1] and belt[i + 1]:
+                robot[i + 1] = 1
+                robot[i] = 0
+                belt[i + 1] -= 1
+            robot[-1] = 0
 
-    # 첫 칸 체크
-    if belt[0]:
-        robot[0] = 1
-        belt[0] -= 1
-    ans += 1
-print(ans)
+        # 첫 칸 체크
+        if belt[0]:
+            robot[0] = 1
+            belt[0] -= 1
+        ans += 1
+    print(ans)
+
+
+if __name__ == "__main__":
+    solution()
+
