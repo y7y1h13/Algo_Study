03be95b9d3e787431f766ca25467d17b26c7b741
HYUNMIN YOO <2049905@naver.com>
2022-06-16 12:33:48 +0900

    15일

---
+++ 카카오, 고득점/15일차/124 나라의 숫자.py
@@ -0,0 +1,13 @@
+def solution(n):
+    answer = ''
+    while n:
+        if n % 3:
+            answer += str(n % 3)
+            n //= 3
+        else:
+            answer += "4"
+            n = n//3 - 1
+    return answer[::-1]
+
+
+print(solution(18))
