625ae0247c8ff2dc119b6ba37854b6faeb7eab82
HYUNMIN YOO <2049905@naver.com>
2022-06-21 08:12:25 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 252 ms, Memory: 48356 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -3,17 +3,24 @@ import sys
 
 input = sys.stdin.readline
 
-N, M = map(int, input().split())
-name = {}
-num = []
-for i in range(N):
-    k = input().rstrip()
-    name[k] = i + 1
-    num.append(k)
 
-for _ in range(M):
-    t = input().rstrip()
-    if t.isalpha():
-        print(name[t])
-    else:
-        print(num[int(t) - 1])
+def solution():
+    N, M = map(int, input().split())
+    name = {}
+    num = []
+    for i in range(N):
+        k = input().rstrip()
+        name[k] = i + 1
+        num.append(k)
+    
+    for _ in range(M):
+        t = input().rstrip()
+        if t.isalpha():
+            print(name[t])
+        else:
+            print(num[int(t) - 1])
+            
+            
+if __name__ == "__main__":
+    solution()
+    
