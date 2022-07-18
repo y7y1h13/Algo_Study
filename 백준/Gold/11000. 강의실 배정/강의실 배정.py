0c7764e62214b810999187ada054bfe501311b2b
HYUNMIN YOO <2049905@naver.com>
2022-07-18 12:31:58 +0900

    [Gold V] Title: 강의실 배정, Time: 444 ms, Memory: 73948 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -4,20 +4,17 @@ from sys import stdin
 input = stdin.readline
 
 
-def solution(N, a):
-    a.sort()
-    r = list()
-    heapq.heappush(r, a[0][1])
-    for i in range(1, N):
-        if a[i][0] >= r[0]:
-            heapq.heappop(r)
-            heapq.heappush(r, a[i][1])
+def solution(a):
+    r = [-1]
+    for s, e in a:
+        if s < r[0]:
+            heapq.heappush(r, e)
         else:
-            heapq.heappush(r, a[i][1])
+            heapq.heapreplace(r, e)
     return len(r)
 
 
 if __name__ == "__main__":
     N = int(input())
-    a = [list(map(int, input().split()))for _ in range(N)]
-    print(solution(N, a))
+    a = sorted([list(map(int, input().split()))for _ in range(N)])
+    print(solution(a))
