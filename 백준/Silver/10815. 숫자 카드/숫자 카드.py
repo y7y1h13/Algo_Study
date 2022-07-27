77f8fce9e3ed943e1b2fad8ec06346a8dab11458
HYUNMIN YOO <2049905@naver.com>
2022-07-27 10:12:54 +0900

    [Silver V] Title: 숫자 카드, Time: 3400 ms, Memory: 111216 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,19 +1,26 @@
-N=int(input())
-card=list(map(int,input().split()))
-card=sorted(card)
-M=int(input())
-target_list=list(map(int,input().split()))
+import sys
 
-def BS(array, target, start, end):
-  if start > end:
-    return 0
-  mid=(start+end)//2
-  if array[mid]==target:
-    return 1
-  elif array[mid]>target:
-    return BS(array, target, start, mid-1)
-  else:
-    return BS(array, target, mid+1, end)
+input = sys.stdin.readline
 
-for target in target_list:
-  print(BS(card, target, 0, N-1), end=' ')
+
+def bi_sec(data, search, start, end):
+    if start > end:
+        return 0
+
+    mid = (start + end) // 2
+    if data[mid] == search:
+        return 1
+    elif data[mid] < search:
+        return bi_sec(data, search, mid + 1, end)
+    else:
+        return bi_sec(data, search, start, mid - 1)
+
+
+if __name__ == "__main__":
+    N = int(input())
+    a = sorted(list(map(int, input().split())))
+    M = int(input())
+    k = list(map(int, input().split()))
+
+    for i in k:
+        print(bi_sec(a, i, 0, N - 1), end=' ')
