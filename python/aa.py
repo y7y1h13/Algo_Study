a7df5d29381a7511e4905fcba9e457affeb2937c
KIEL <2049905@naver.com>
2022-08-21 22:33:17 +0900

    delete

--- python/aa.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-
-sys.path.append('.\\test')
-import module
-
-print(module.add(4, 4))
--- python/test/module.py
+++
@@ -1,6 +0,0 @@
-def add(x, y):
-    return x * y
-
-
-if __name__ == "__main__":
-    print(add(3, 4))
