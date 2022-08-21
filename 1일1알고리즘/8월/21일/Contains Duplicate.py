2f860950792865906898a53ffa8897dd16c109bc
KIEL <2049905@naver.com>
2022-08-21 22:33:42 +0900

    8월21일

---
+++ 1일1알고리즘/8월/21일/Contains Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums):
+        a = set()
+        for i in nums:
+            if i in a:
+                return True
+            a.add(i)
+        return False
