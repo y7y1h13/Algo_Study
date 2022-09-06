72853a5a740e24ffadbf43201cbec6ccd6ca48f9
HYUNMIN YOO <2049905@naver.com>
2022-09-06 09:37:43 +0900

    [Silver IV] Title: 카드2, Time: 240 ms, Memory: 53356 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -2,13 +2,9 @@ from collections import deque
 
 
 N = int(input())
-q = deque()
-
-for i in range(1, N + 1):
-    q.append(i)
-l = N
-while l > 1:
-    q.popleft()
-    l -= 1
-    q.append(q.popleft())
-print(*q)
+a = deque([i for i in range(1, N + 1)])
+while N > 1:
+    a.popleft()
+    a.append(a.popleft())
+    N -= 1
+print(*a)
