9f58468f206bc86a6b2a1145e86566535a68d8b0
HYUNMIN YOO <2049905@naver.com>
2022-09-10 10:50:38 +0900

    [Silver II] Title: 최대 힙, Time: 196 ms, Memory: 45980 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.py
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -3,14 +3,11 @@ import sys
 
 input = sys.stdin.readline
 a = list()
-l = 0
 for _ in range(int(input())):
     n = int(input())
     if n == 0:
-        if l == 0:
+        if len(a) == 0:
             print(0)
         else:
-            l -= 1
             print(heapq.heappop(a)[1])
     heapq.heappush(a, (-n, n))
-    l += 1
