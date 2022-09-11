202aeda27c3468d7819f616354729d213a7d83c0
ㅁin <2049905@naver.com>
2022-09-11 11:01:13 +0900

    9월10일

---
+++ 1일1알고리즘/9월/10일/카드 정렬하기.py
@@ -0,0 +1,24 @@
+import heapq
+
+n = int(input())
+heap = list(int(input()) for _ in range(n))
+heapq.heapify(heap)
+l, tmp, ans = 0, 0, 0
+if n == 1:
+    print(ans)
+    exit(0)
+while True:
+    if len(heap) == 1:
+        tmp += heapq.heappop(heap)
+        ans += tmp
+        print(ans)
+        exit(0)
+    else:
+        if l == 1:
+            tmp += heapq.heappop(heap)
+            heapq.heappush(heap, tmp)
+            ans += tmp
+            l, tmp = 0, 0
+        else:
+            tmp = heapq.heappop(heap)
+            l += 1
