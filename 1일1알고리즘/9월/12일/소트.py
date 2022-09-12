f5c351e6ec7e761692b4535f33c2afadf8cd4d03
ㅁin <2049905@naver.com>
2022-09-12 11:38:16 +0900

    9월12일

---
+++ 1일1알고리즘/9월/12일/소트.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def max_n(inner_idx):
+    tmp, idx = a[inner_idx], inner_idx
+    for i in range(inner_idx + 1, min(n, inner_idx + s + 1)):
+        if a[i] > tmp:
+            tmp = a[i]
+            idx = i
+    return tmp, idx
+
+
+if __name__ == "__main__":
+    n = int(input())
+    a = list(map(int, input().split()))
+    s = int(input())
+    for i in range(n - 1):
+        max_Num, max_Idx = max_n(i)
+        if max_Idx != i:
+            del a[max_Idx]
+            a.insert(i, max_Num)
+            s -= (max_Idx - i)
+    print(*a)
